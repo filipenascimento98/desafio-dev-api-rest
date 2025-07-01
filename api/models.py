24ea59be20f14a613454c56368a880a115335acd
Filipe Nascimento <filipe.almeida@dcomp.ufs.br>
2025-07-01 11:18:47 -0300

    fix: Atualizando nomes dos campos nos models e adicionando algumas configurações nos mesmos

--- api/models.py
+++ api/models.py
@@ -2,29 +2,29 @@ from django.db import models
 
 
 class Portador(models.Model):
-    nome_completo = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, primary_key=True)
 
 
-class ContaDigital(models.Model):
+class DigitalAccount(models.Model):
     portador = models.OneToOneField(
         Portador,
         related_name='conta_digital',
         on_delete=models.DO_NOTHING
     )
-    saldo_atual = models.FloatField()
-    numero = models.IntegerField()
-    agencia = models.IntegerField()
-    ativa = models.BooleanField(default=False)
-    bloqueada = models.BooleanField(default=False)
+    current_balance = models.FloatField()
+    number = models.IntegerField()
+    agency = models.IntegerField()
+    active = models.BooleanField(default=True)
+    blocked = models.BooleanField(default=True)
 
 
-class Transacao(models.Model):
-    conta_digital = models.ForeignKey(
-        ContaDigital,
-        related_name='transacao',
+class Transaction(models.Model):
+    digital_account = models.ForeignKey(
+        DigitalAccount,
+        related_name='transaction',
         on_delete=models.DO_NOTHING
     )
-    valor = models.FloatField()
-    data = models.DateField()
-    tipo_transacao = models.CharField(choices=(('saque', 'Saque'), ('deposito', 'Deposito')))
+    value = models.FloatField()
+    transaction_type = models.CharField(choices=(('withdraw', 'Withdraw'), ('deposit', 'Deposit')))
+    created_at = models.DateField(auto_now_add=True)
